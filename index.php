e5b5e75a8e088c700566c5e72ac1c3fec30e2742
11550066 <>
2020-01-31 12:06:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>Fotoladia by Free Css Templates</title>
+<title>Prodavalnik</title>
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <link href="default.css" rel="stylesheet" type="text/css" />
