7c06ad2a26a7fcf4a6b2e39f183128b7b14fc3ba
11550066 <60502559+11550066@users.noreply.github.com>
2020-01-31 11:32:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE php PUBLIC>
+<!--
+
+-->
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Fotoladia by Free Css Templates</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="default.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+	<h1>Продавалник</h1>
+	<h2>за хубави стоки</h2>
+</div>
+<div id="content">
+	<div id="colOne">
+		<h2> Добре дошли в продавалника!</h2>
+		<p1>Примерен неподреден списък на стоките</p1>
+		<ul>
+		  <li><a href="#">Промишлени стоки;</a></li>
+			<li><a href="#">Стоки за бита;</a></li>
+			<li><a href="#">Стоки за жената;</a></li>
+			<li><a href="#">Инструменти и стоки за мъжа;</a></li>
+			<li><a href="#">Спортни и детски стоки.</a></li>
+	  </ul>
+		<div class="credits">
+			<p class="file"><small>January 30th, 2020  by </small>bd (under construction)</p>
+			<p class="posted">Posted in <a href="#">Classroom</a> | <a href="#">Edit</a> | <a href="#">Comment</a></p>
+		</div>
+		<h3>Условия за ползване на Продавалника:</h3>
+		<p>Безплатно при следни условия:</p>
+		<ol>
+			<li><a href="#">Стоките да са с български произход или собственик.</a></li>
+			<li><a href="#">Продавача гарантира качеството, произхода, собствеността и връщането обратно на стоката от купувача в рамните на мин. 15 дни.</a></li>
+			<li><a href="#">Стоките да са в рамките на разрешените за търгуване стоки съгласно българското законателство.</a></li>
+		</ol>
+		<div class="credits">
+			<p class="file"><small>January 30th, 2020  by </small>bd (under construction)</p>
+            <p class="posted">Posted in <a href="#">Classroom</a> | <a href="#">Edit</a> | <a href="#">Comment</a></p>
+		</div>
+	</div>
+	<div id="colTwo">
+		<ul>
+			<li>
+				<form id="form1" method="get" action="#">
+					<div>
+						<input type="text" id="textfield1" name="textfield1" value="" size="18" />
+						<input type="submit" id="submit1" name="submit1" value="Търси" />
+					</div>
+				</form>
+			</li>
+			<li>
+				<h2>Страници</h2>
+				<ul>
+					<li class="active"><a href="#">Нашите писания</a></li>
+					<li><a href="#">Нашите снимки</a></li>
+					<li><a href="#">Нашите приятели</a></li>
+					<li><a href="#">За нас</a></li>
+					<li><a href="#">Контакти и адреси</a></li>
+				</ul>
+			</li>
+			<li>
+				<h2>Архив</h2>
+				<ul>
+					<li><a href="#">февруари 2019</a></li>
+					<li><a href="#">март 2019</a></li>
+					<li><a href="#">април 2019</a></li>
+					<li><a href="#">май  2019</a></li>
+					<li><a href="#">юни 2019</a></li>
+					<li><a href="#">юли 2019</a></li>
+					<li><a href="#">август 2019</a></li>
+					<li><a href="#">септември 2019</a></li>
+					<li><a href="#">октомнри 2019</a></li>
+					<li><a href="#">ноември 2019</a></li>
+					<li><a href="#">декември 2019</a></li>
+				</ul>
+			</li>
+			<li>
+				<h2>Категории</h2>
+				<ul>
+					<li><a href="#">Промишлени стоки</a><a href="#"></a> (23)</li>
+					<li><a href="#">Стоки за бита</a><a href="#"></a> (78)</li>
+					<li><a href="#">Стоки за жената</a><a href="#"></a> (11)</li>
+					<li><a href="#">Инструменти и стоки за мъжа</a><a href="#"></a> (34)</li>
+					<li><a href="#">Спортни и детски стоки</a><a href="#"></a> (65)</li>
+					<li><a href="#">Други</a><a href="#"></a> (35)</li>
+				</ul>
+			</li>
+			<li>
+				<h2>Връзки с други страници</h2>
+				<ul>
+					<li><a href="#">Goods1.com</a></li>
+					<li><a href="#">Goods2.net</a></li>
+					<li><a href="#">Goods3.com</a></li>
+					<li><a href="#">Goods4.bg</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
+<div id="footer">
+	<p>&nbsp;</p>
+</div>
+<div align=center></div></body>
+</html>
